b6247860b6accd891b5b8565c4d75ee701f43021
Sid1608 <akarsiddharth@gmail.com>
2021-08-27 23:06:09 +0530

    Added Level 3 Authentication - Hashing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express= require ("express");
 const bodyParser= require("body-parser");
 const ejs=require("ejs");
 const mongoose =require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5=require('md5');
 
 /**App setup */
 const app = express();
@@ -26,7 +26,7 @@ const userSchema=new mongoose.Schema({
 
 /**Encrypting password field */
 const secret=process.env.SECRET;
-userSchema.plugin(encrypt,{secret:secret, encryptedFields: ["password"]});
+
 
 const User=new mongoose.model("User",userSchema);
 
@@ -48,7 +48,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     })
     newUser.save(function(err){
         if(err){
@@ -62,7 +62,7 @@ app.post("/register",function(req,res){
 /**post route for login page */
 app.post("/login",function(req,res){
     const username= req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
 
     User.findOne({email:username},function(err,foundUser){
         if(err){
