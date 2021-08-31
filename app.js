1fbd650bb56364dad2af9d7a610947db3bcdef67
Sid1608 <akarsiddharth@gmail.com>
2021-08-31 20:30:32 +0530

    Added Level 5 Authentication - Cookies and Sessions

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const express= require ("express");
 const bodyParser= require("body-parser");
 const ejs=require("ejs");
 const mongoose =require("mongoose");
-const bcrypt=require('bcrypt');
-const saltRounds =10;
+const session=require("express-session");
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
+
 
 /**App setup */
 const app = express();
@@ -14,10 +16,21 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended:true
 }));
-/*console.log(process.env.API_KEY);*/
+
+/* Setting up session */
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+/* initializing passport */
+app.use(passport.initialize());
+app.use(passport.session()); 
+
 /**Setting up Connection */
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
-
+// mongoose.set("useCreateIndex", true);
 
 /**User-Schema */
 const userSchema=new mongoose.Schema({
@@ -25,11 +38,15 @@ const userSchema=new mongoose.Schema({
     password:String
 });
 
-/**Encrypting password field */
-const secret=process.env.SECRET;
-
+/*setting up passport-local-mongoose */
+userSchema.plugin(passportLocalMongoose);/* to hash and salt the passwords and to save user into mongoDB database */
 
 const User=new mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 /**Home Route */
 app.get("/",function(req,res){
@@ -45,44 +62,52 @@ app.get("/register",function(req,res){
     res.render("register");
 });
 
+/*get route for secrets page */
+app.get("/secrets",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+});
+
+/*get route for logout page */
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/");
+});
 /**post route for register page */
 app.post("/register",function(req,res){
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        const newUser=new User({
-            email:req.body.username,
-            password: hash
-        })
-        newUser.save(function(err){
-            if(err){
-                console.log(err);
-            }else{
-                res.render("secrets");
-            }
-        });
+    /*This method comes from passport-local-mongoose package */
+    User.register({username:req.body.username}, req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            /*Authenticating user */
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
+        }
     });
     
 });
 
 /**post route for login page */
 app.post("/login",function(req,res){
-    const username= req.body.username;
-    const password=req.body.password;
-
-    User.findOne({email:username},function(err,foundUser){
+    const user=new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user,function(err){
         if(err){
             console.log(err);
         }else{
-            if(foundUser){ 
-                bcrypt.compare(password,foundUser.password, function(err, result) {
-                    if(result=== true){
-                        res.render("secrets");
-                    }
-                });
-    
-            }
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
         }
-    });
-
+    })
 })
 
 /**Server listening on port 3000 */
